ecd19cd49264fd0b03caa596e17050349ce3e19e
eXtrever <dmitriy.kochkarev@gmail.com>
2022-12-07 12:49:04 -0400

    Added /list method. Need to fix output string formating

--- PhoneBookConsoleApp/Program.cs
+++ PhoneBookConsoleApp/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            string DbPath = Directory.GetCurrentDirectory() + "\\Phonebook.csv";
             string cmd = "";
             string[] CmdInput = { "/help", "/exit", "/list", "/search", "/add", "/delete", "/edit" };
             List<string> Commands = new List<string>(CmdInput);
@@ -23,6 +24,10 @@
 
                         case "/exit":
                             break;
+
+                        case "/list":
+                            ListPhoneBook(DbPath);
+                            break;
                     }
                 }
                 else
@@ -32,6 +37,51 @@
                 }
 
             } while (cmd != "/exit");
+        }
+
+        private static void ListPhoneBook(string Path)
+        {           
+            if (File.Exists(Path))
+            {
+                Console.Clear();
+                Console.WriteLine("Pnone Book App v0.0.1 - PhoneBook Listing");
+                Console.WriteLine();                                
+
+                string[] lines= File.ReadAllLines(Path);
+                
+                //Check how many rows and columns in the CSV file
+                int rows_num = lines.Length;
+                int cols_num = lines[0].Split(',').Length;
+
+                //Allocate the array to load the CSV file
+                string[,] csv_values = new string[rows_num, cols_num];
+
+                //Load the array
+                for (int r = 0; r < rows_num; r++)
+                {
+                    string[] line_r = lines[r].Split(',');
+
+                    for (int c = 0; c < cols_num; c++)
+                    {
+                        csv_values[r, c] = line_r[c];
+                    }
+
+                }
+
+                for (int r = 0; r < csv_values.GetLength(0); r++)
+                {
+                    for (int c = 0; c < csv_values.GetLength(1); c++)
+                    {
+                        // TODO: fix string formating - pipes and alignment
+                        Console.Write("{0, -15} {1, 5}", csv_values[r, c], "|");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Pres any key to return to the main menu.");
+                Console.ReadKey();
+            }
         }
 
         private static void Help()
