0a7bdea9a4d00bb14ade4a025f7c14d59339a912
eXtrever <dmitriy.kochkarev@gmail.com>
2022-12-07 10:42:03 -0400

    Added 'exit' and empty 'help' commands

--- PhoneBookConsoleApp/Program.cs
+++ PhoneBookConsoleApp/Program.cs
@@ -5,13 +5,44 @@
         static void Main(string[] args)
         {
             string cmd = "";
-
+            string[] CmdInput = { "/help", "/exit", "/list", "/search", "/add", "/delete", "/edit" };
+            List<string> Commands = new List<string>(CmdInput);
+            
             do
             {
                 BasicGreeting();
-
+                
                 cmd = Console.ReadLine();
+                if (Commands.Contains(cmd))
+                {
+                    switch(cmd)
+                    {
+                        case "/help":
+                            Help();
+                            break;
+
+                        case "/exit":
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong command, try again. Press any key to continue.");
+                    Console.ReadKey();
+                }
+
             } while (cmd != "/exit");
+        }
+
+        private static void Help()
+        {
+            Console.Clear();
+            Console.WriteLine("Pnone Book App v0.0.1 - Help");
+            Console.WriteLine();
+            Console.WriteLine("Here will be a help text...");
+            Console.WriteLine();
+            Console.WriteLine("Pres any key to return to the main menu.");
+            Console.ReadKey();
         }
 
         private static void BasicGreeting()
