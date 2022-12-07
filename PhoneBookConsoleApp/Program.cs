3b07c5ffa3adec42365c09214593e8580650a30b
eXtrever <dmitriy.kochkarev@gmail.com>
2022-12-07 09:18:12 -0400

    Initial commit

---
+++ PhoneBookConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PhoneBookConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cmd = "";
+
+            do
+            {
+                BasicGreeting();
+
+                cmd = Console.ReadLine();
+            } while (cmd != "/exit");
+        }
+
+        private static void BasicGreeting()
+        {
+            Console.Clear();
+            Console.WriteLine("Pnone Book App v0.0.1");
+            Console.WriteLine();
+            Console.WriteLine($"Please enter your command or type /help for help.");
+        }
+    }
+}
